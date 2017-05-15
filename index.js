6e1c7b880dfe0e937a859f48330a9e7e058b8663
Charley Drew-Wolak <charlzahk@gmail.com>
2017-05-15 16:04:34 -0400

    before messing up

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const personForm = document.querySelector('form')
-console.log(personForm)
 
-personForm.onsubmit = () => {
-    ev.prefentDefault()
-    console.log(ev)
+personForm.onsubmit = (ev) => {
+  ev.preventDefault()
+  const form = ev.target
+  const details = document.querySelector('.details')
+
+  const personName = form.personName.value
+  const hairColor = form.hairColor.value
+
+  const em = document.createElement('em')
+  em.textContent = personName
+
+  const colorDiv = document.createElement('div')
+  colorDiv.style.backgroundColor = hairColor
+  colorDiv.style.height = '50px'
+  colorDiv.style.width = '100px'
+
+  details.appendChild(em)
+  details.appendChild(colorDiv)
 }

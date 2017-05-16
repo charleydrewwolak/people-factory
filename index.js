a1fec5e9b46d76dfa069e127de61a94a0e8fe5e0
Charley Drew-Wolak <charlzahk@gmail.com>
2017-05-16 09:54:36 -0400

    finished

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
 const personForm = document.querySelector('form')
 
-personForm.onsubmit = (ev) => {
-  ev.preventDefault()
-  const form = ev.target
-  const details = document.querySelector('.details')
-
-  const personName = form.personName.value
-  const hairColor = form.hairColor.value
-
-  const em = document.createElement('em')
-  em.textContent = personName
-
+const renderColor = (hairColor) => {
   const colorDiv = document.createElement('div')
   colorDiv.style.backgroundColor = hairColor
   colorDiv.style.height = '50px'
   colorDiv.style.width = '100px'
-
-  details.appendChild(em)
-  details.appendChild(colorDiv)
+  return colorDiv
 }
+
+
+const handleSubmit = (ev) => {
+  ev.preventDefault()
+  const form = ev.target
+  const details = document.querySelector('.details')
+
+ 
+  const personName = document.createElement('li')
+  personName.textContent = form.personName.value
+
+  const hairColor = form.hairColor.value
+
+  const color = document.createElement('li')
+  color.textContent = 'Color:'
+
+  const age = document.createElement('li')
+  age.textContent = form.age.value
+
+  const birthplace = document.createElement('li')
+  birthplace.textContent = form.birthplace.value
+
+  const colorDiv = renderColor(hairColor)
+
+
+  details.appendChild(personName)
+  details.appendChild(color)
+  details.appendChild(colorDiv)
+  details.appendChild(age)
+  details.appendChild(birthplace)
+  
+}
+
+personForm.addEventListener('submit', handleSubmit)
